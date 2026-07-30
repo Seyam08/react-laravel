71720bc6c6bff94c60f4a0a638a16eef3a40901f
Seyam <seyam.hossain.118833@gmail.com>
2026-07-30 18:26:03 +0600

    refactor(policy): comment out unused policy methods for clarity

--- app/Policies/BookPolicy.php
+++ app/Policies/BookPolicy.php
@@ -11,26 +11,26 @@ class BookPolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
-    {
-        return false;
-    }
+    // public function viewAny(User $user): bool
+    // {
+    //     return false;
+    // }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Book $book): bool
-    {
-        return false;
-    }
+    // public function view(User $user, Book $book): bool
+    // {
+    //     return false;
+    // }
 
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user): bool
-    {
-        return false;
-    }
+    // public function create(User $user): bool
+    // {
+    //     return false;
+    // }
 
     /**
      * Determine whether the user can update the model.
@@ -51,16 +51,16 @@ class BookPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Book $book): bool
-    {
-        return false;
-    }
+    // public function restore(User $user, Book $book): bool
+    // {
+    //     return false;
+    // }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Book $book): bool
-    {
-        return false;
-    }
+    // public function forceDelete(User $user, Book $book): bool
+    // {
+    //     return false;
+    // }
 }
